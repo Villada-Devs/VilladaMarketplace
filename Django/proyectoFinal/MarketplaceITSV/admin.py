b459fa1b2a62c74a96e846112e2e5f47543bde29
Manuel Ferreras <35044975+ManuelFerreras@users.noreply.github.com>
2022-04-22 13:05:41 -0300

    Revert "paco helaaaaoooo"

--- Django/proyectoFinal/MarketplaceITSV/admin.py
+++ Django/proyectoFinal/MarketplaceITSV/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import *
-from django.contrib.auth.models import Permission
 
-admin.site.register(Post)
-#admin.site.register(Permission)
-
+# Register your models here.
--- Django/proyectoFinal/config/settings.py
+++ Django/proyectoFinal/config/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os
 
 from pathlib import Path
 
@@ -118,13 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
