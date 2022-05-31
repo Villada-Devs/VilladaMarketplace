260c5b2a2d30d094ca4927fc90dcc7822b57e732
Paco Pairone <pacopairone@gmail.com>
2022-05-31 15:01:59 +0000

    Update models.py

--- Django/proyectoFinal/MarketplaceITSV/models.py
+++ Django/proyectoFinal/MarketplaceITSV/models.py
@@ -1,48 +1,55 @@
 from unittest.util import _MAX_LENGTH
 from django.db import models
-from django.db.models.fields import CharField, SlugField, DateTimeField, TextField, DateField
+from django.db.models.fields import CharField, SlugField, DateTimeField, TextField, DateField, IntegerField
 
 # Create your models here.
 
-class Post(models.Model):
-    title = models.CharField(max_length=255)
+class Blog(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=30)
     body = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
-    imagen = models.ImageField(upload_to= "images")
+    created_date = models.DateTimeField(auto_now_add=True)
+    
 
-    def __str__(self):
-        return f"{self.title}" 
-
-    class Meta:
-        ordering = ['-date']
-
-
-class CommentPost(models.Model):
-    post = models.ForeignKey('Post', related_name= 'comments',on_delete=models.CASCADE)
-    comentario = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
-
-#nose que es el "related_name= 'comments'" en la conexion de foreign key de post
-    class Meta:
-        ordering = ['-date']
-
-class Event(models.Model):
-    title = models.CharField(max_length=255)
-    body = models.TextField(blank=True, null=True)
-    date = models.DateTimeField(auto_now_add=True)
-    set_date = models.DateField()
-    image = models.ImageField(upload_to= "images")
+class Comentario(models.Model):
+    id = models.IntegerField(primary_key=True)
+    blog_id = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    created_by = 
+    body = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
 
 
-    def __str__(self):
-        return f"{self.title}" 
+class Evento(models.Model):
+    id = 
+    create_by = 
+    title = 
+    body = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    image = 
 
-    class Meta:
-        ordering = ['-date']
 
-class CommentEvent(models.Model):
-    event = models.ForeignKey('Event', related_name= 'comments',on_delete=models.CASCADE)
-    comentario = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
+class Pool(models.Model):
+    id = 
+    created_by = 
+    zone = 
+    slots = 
+    days = 
+    tel = 
+
+
+class Listado(models.Model):
+    id = 
+    created_by = 
+    creation_date = models.DateTimeField(auto_now_add=True)
+    expiration_date = 
+    item = 
+    description = 
+    price = 
+    tel = 
+
+
+    
+    
+    
 
 
