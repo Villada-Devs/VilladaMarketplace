74ef4fc65b2e06b7d6c3842ddfec59d41e95cf8a
Matias-C <55718713+Matias-C@users.noreply.github.com>
2022-08-17 08:22:13 -0300

    Update LandPage.js

--- src/components/LandPage.js
+++ src/components/LandPage.js
@@ -14,7 +14,7 @@ function LandPage() {
             <div className="top-container">
 
                 <div className="left-card">
-                    <h1 className="top-title">ejnndw <br></br>a la <span>Unión <br></br>Padres de Familia.</span></h1>
+                    <h1 className="top-title">Bienvenidos <br></br>a la <span>Unión <br></br>Padres de Familia.</span></h1>
                     <p className="sub-title">Aquí puedes actualizarte con las últimas noticias del colegio, y comprar útiles, libros y herramientas.</p>
                     <button type="button" className="btn btn-marketplace">Marketplace</button>
                     <p className="texto-conoce-mas">Conocé más sobre nosotros  <FontAwesomeIcon className="conocer-link" icon={faArrowRight} /></p>
