771c782c65f7a2fa2bd009d40430a58ea8488f77
marcosdon28 <marcosdon28@gmail.com>
2022-11-11 13:00:42 -0300

    add pool login validation

--- src/components/marketplace/MarketplaceMain.js
+++ src/components/marketplace/MarketplaceMain.js
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, {useContext} from "react";
+import ContextConnected from "../../context/ContextConnected";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
@@ -13,46 +13,59 @@ import UniformsSection from "../../img/marketplace/UniformsSectionImg.png";
 import "../../styles/marketplace/MarketplaceMain.css";
 
 function MarketplaceMain() {
-    return (
-
-        <>
-
-            <Container className="page-container" fluid>
-                <PageHeader
-                    pageHeader="Villada Marketplace"
-                    pageDescription="Publicá, comprá o vendé herramientas, libros y uniformes que no utilices más."
-                    button="Vender"
-                />
-
-                <div className="marketplace-categories">
-
-                    <p className="categories-header">Comprar por Categoría</p>
-
-                </div>
-
-                <Row>
-                    <SectionButton 
-                        sectionImage={BooksSection}
-                        sectionName="Libros y Apuntes"
-                        sectionURL="Libros"
+    const Connected = useContext(ContextConnected);
+    return ( 
+        <Container className="page-container" fluid>
+            {Connected.userInfo? (
+                <>
+                    <PageHeader
+                        pageHeader="Villada Marketplace"
+                        pageDescription="Publicá, comprá o vendé herramientas, libros y uniformes que no utilices más."
+                        button="Vender"
                     />
 
-                    <SectionButton 
-                        sectionImage={ToolsSection}
-                        sectionName="Herramientas"
-                        sectionURL="Herramientas"
+                    <div className="marketplace-categories">
+
+                        <p className="categories-header">Comprar por Categoría</p>
+
+                    </div>
+
+                    <Row>
+                        <SectionButton 
+                            sectionImage={BooksSection}
+                            sectionName="Libros y Apuntes"
+                            sectionURL="Libros"
+                        />
+
+                        <SectionButton 
+                            sectionImage={ToolsSection}
+                            sectionName="Herramientas"
+                            sectionURL="Herramientas"
+                        />
+
+                        <SectionButton 
+                            sectionImage={UniformsSection}
+                            sectionName="Uniformes"
+                            sectionURL="Uniformes"  
+                        />
+                    </Row>
+                </>
+            ) :
+            (
+                <>
+                    <PageHeader
+                        pageHeader="Villada Marketplace"
+                        pageDescription="Publicá, comprá o vendé herramientas, libros y uniformes que no utilices más."
+                        button="Vender"
                     />
 
-                    <SectionButton 
-                        sectionImage={UniformsSection}
-                        sectionName="Uniformes"
-                        sectionURL="Uniformes"  
-                    />
-                </Row>
+                    <div className="marketplace-categories">
+                        <p className="categories-header">Inicia sesión y comenzá a publicar !</p>
+                    </div>
 
-            </Container>
-            
-        </>
+                </>
+            )}
+        </Container>
 
     );
 }
