1309c1aa78d619792eafeb521dbcbd6937effba8
ManuelFerreras <manuel.ferreras1@gmail.com>
2022-11-23 09:56:57 -0300

    form

--- src/components/marketplace/MarketplaceForm.js
+++ src/components/marketplace/MarketplaceForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
@@ -14,21 +14,29 @@ import ToolsSection from "../../img/marketplace/ToolsSectionImg.png";
 import UniformsSection from "../../img/marketplace/UniformsSectionImg.png";
 
 import "../../styles/marketplace/MarketplaceForm.css"
+import { useNavigate } from "react-router-dom";
+import ContextConnected from "../../context/ContextConnected";
+
+const booksPostUrl = 'http://villadaapidjango-env.eba-vaws9zih.us-east-1.elasticbeanstalk.com/api/v1/marketplace/books/';
+const toolsPostUrl = 'http://villadaapidjango-env.eba-vaws9zih.us-east-1.elasticbeanstalk.com/api/v1/marketplace/tools/';
+const clothsPostUrl = 'http://villadaapidjango-env.eba-vaws9zih.us-east-1.elasticbeanstalk.com/api/v1/marketplace/clothes/';
 
 function MarketplaceForm() {
+    const navigate = useNavigate();
+    const context = useContext(ContextConnected);
 
     const [articleType, setArticleType] = useState(null)
 
     const [validated, setValidated] = useState(false);
 
     const submit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-    }
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
 
-    setValidated(true);
+        setValidated(true);
     };
 
     const [sendImages, setSendImages] = useState([]);
@@ -37,6 +45,152 @@ function MarketplaceForm() {
 
     const sendEvent = async (event) => {
         event.preventDefault();
+
+        if(articleType === null){
+            alert("Please select an article type")
+            return
+        }
+
+        context.setSpinnerShowing(true);
+
+        switch(articleType) {
+            case 0:
+                await sendBook();
+                context.setSpinnerShowing(false)
+                break;
+
+            case 1:
+                await sendTool();
+                context.setSpinnerShowing(false)
+                break;
+
+            case 2:
+                await sendCloth();
+                context.setSpinnerShowing(false)
+                break;
+
+            default:
+                alert("Please select an article type");
+                context.setSpinnerShowing(false)
+                return
+        }
+        
+    }
+
+    const sendBook = async () => {
+        const token = await JSON.parse(localStorage.getItem("token"));
+        if (token) {
+            const title = document.querySelector("#book-title").value;
+            const author = document.querySelector("#book-author").value;
+            const editorial = document.querySelector("#book-editorial").value;
+            const materia = document.querySelector("#book-subject").value;
+            const curso = document.querySelector("#book-course").value;
+            const estado = document.querySelector("#book-state").value;
+            const precio = document.querySelector("#book-price").value;
+            const tel = document.querySelector("#book-tel").value;
+
+            const props = {
+                author: author,
+                subject: materia,
+                course: curso,
+                editorial: editorial
+            }
+
+            var formdata = new FormData();
+            formdata.append("product_name", title);
+            formdata.append("props", JSON.stringify(props));
+            formdata.append("status", estado);
+            formdata.append("price", precio);
+            formdata.append("tel", tel);
+
+            for (let i = 0; i < selectedImages.length; i++) {
+                formdata.append('uploaded_images', selectedImages[i]);
+            };
+
+            await fetch(booksPostUrl, {
+                method: "POST",
+                headers: {
+                    "Authorization": `Bearer ${token.access_token}`
+                },
+                body: formdata
+            }).then(() => {
+                navigate("/Marketplace");
+            })
+        }
+    }
+
+    const sendTool = async () => {
+        const token = await JSON.parse(localStorage.getItem("token"));
+        if (token) {
+            const title = document.querySelector("#tool-title").value;
+            const estado = document.querySelector("#tool-state").value;
+            const descripcion = document.querySelector("#tool-description").value;
+            const precio = document.querySelector("#tool-price").value;
+            const tel = document.querySelector("#tool-tel").value;
+
+            const props = {
+                description: descripcion
+            }
+
+            var formdata = new FormData();
+            formdata.append("product_name", title);
+            formdata.append("props", JSON.stringify(props));
+            formdata.append("status", estado);
+            formdata.append("price", precio);
+            formdata.append("tel", tel);
+
+            for (let i = 0; i < selectedImages.length; i++) {
+                formdata.append('uploaded_images', selectedImages[i]);
+            };
+
+            await fetch(toolsPostUrl, {
+                method: "POST",
+                headers: {
+                    "Authorization": `Bearer ${token.access_token}`
+                },
+                body: formdata
+            }).then(() => {
+                navigate("/Marketplace");
+            })
+        }
+    }
+
+    const sendCloth = async () => {
+        const token = await JSON.parse(localStorage.getItem("token"));
+        if (token) {
+            const title = document.querySelector("#uniform-title").value;
+            const estado = document.querySelector("#uniform-state").value;
+            const descripcion = document.querySelector("#uniform-description").value;
+            const size = document.querySelector("#uniform-size").value;
+            const precio = document.querySelector("#uniform-price").value;
+            const tel = document.querySelector("#uniform-tel").value;
+
+            const props = {
+                size: size,
+                description: descripcion
+            }
+
+            var formdata = new FormData();
+            formdata.append("product_name", title);
+            formdata.append("props", JSON.stringify(props));
+            formdata.append("status", estado);
+            formdata.append("price", precio);
+            formdata.append("tel", tel);
+
+            for (let i = 0; i < selectedImages.length; i++) {
+                formdata.append('uploaded_images', selectedImages[i]);
+            };
+
+            await fetch(toolsPostUrl, {
+                method: "POST",
+                headers: {
+                    "Authorization": `Bearer ${token.access_token}`
+                },
+                body: formdata
+            }).then(() => {
+                navigate("/Marketplace");
+            })
+        }
     }
 
     return (
@@ -115,7 +269,7 @@ function MarketplaceForm() {
 
                                         <Form.Group as={Col} md="4">
                                             <Form.Label className='input-label'>Materia</Form.Label>
-                                            <Form.Select required className='input' id='type'>
+                                            <Form.Select required className='input' id='book-subject'>
                                                 <option value=""></option>
                                                 <option value="language">Lengua</option>
                                                 <option value="math">Matemáticas</option>
@@ -131,7 +285,7 @@ function MarketplaceForm() {
 
                                         <Form.Group as={Col} md="4">
                                             <Form.Label className='input-label'>Curso</Form.Label>
-                                            <Form.Select required className='input' id='book-curse'>
+                                            <Form.Select required className='input' id='book-course'>
                                                 <option value=""></option>
                                                 <option value="1">1°</option>
                                                 <option value="2">2°</option>
@@ -159,6 +313,24 @@ function MarketplaceForm() {
                                             </Form.Control.Feedback>
                                         </Form.Group>
 
+                                        <Form.Group as={Col} md="6">
+                                            <Form.Label className='input-label'>Telefono</Form.Label>
+                                            <Form.Control
+                                                className='input'
+                                                type="text"
+                                                id='book-tel'
+                                            />
+                                        </Form.Group>
+
+                                        <Form.Group as={Col} md="6">
+                                            <Form.Label className='input-label'>Precio</Form.Label>
+                                            <Form.Control
+                                                className='input'
+                                                type="text"
+                                                id='book-price'
+                                            />
+                                        </Form.Group>
+
                                     </Row>
 
                                     <Button className='button' onClick={(e) => sendEvent(e)}>Publicar</Button>
@@ -184,7 +356,7 @@ function MarketplaceForm() {
                                         <Form.Control
                                             className='input'
                                             type="text"
-                                            id='tool'
+                                            id='tool-title'
                                             required
                                         />
                                         <Form.Control.Feedback type="invalid">
@@ -217,6 +389,24 @@ function MarketplaceForm() {
                                         />
                                     </Form.Group>
 
+                                    <Form.Group as={Col} md="6">
+                                        <Form.Label className='input-label'>Telefono</Form.Label>
+                                        <Form.Control
+                                            className='input'
+                                            type="text"
+                                            id='tool-tel'
+                                        />
+                                    </Form.Group>
+
+                                    <Form.Group as={Col} md="6">
+                                        <Form.Label className='input-label'>Precio</Form.Label>
+                                        <Form.Control
+                                            className='input'
+                                            type="text"
+                                            id='tool-price'
+                                        />
+                                    </Form.Group>
+
                                 </Row>
 
                                 <Button className='button' onClick={(e) => sendEvent(e)}>Publicar</Button>
@@ -242,7 +432,7 @@ function MarketplaceForm() {
                                             <Form.Control
                                                 className='input'
                                                 type="text"
-                                                id='uniform'
+                                                id='uniform-title'
                                                 required
                                             />
                                             <Form.Control.Feedback type="invalid">
@@ -288,6 +478,24 @@ function MarketplaceForm() {
                                                 rows={3} 
                                                 id="uniform-description"
                                                 placeholder="Opcional" 
+                                            />
+                                        </Form.Group>
+
+                                        <Form.Group as={Col} md="6">
+                                            <Form.Label className='input-label'>Telefono</Form.Label>
+                                            <Form.Control
+                                                className='input'
+                                                type="text"
+                                                id='uniform-tel'
+                                            />
+                                        </Form.Group>
+
+                                        <Form.Group as={Col} md="6">
+                                            <Form.Label className='input-label'>Precio</Form.Label>
+                                            <Form.Control
+                                                className='input'
+                                                type="text"
+                                                id='uniform-price'
                                             />
                                         </Form.Group>
 
